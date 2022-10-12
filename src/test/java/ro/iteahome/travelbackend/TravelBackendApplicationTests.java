271ecd731ab855cbf17a1e5b63d1dcb54d25c834
ursanbia <ursan.bia@gmail.com>
2022-10-12 16:00:51 +0300

    Inital commit

---
+++ src/test/java/ro/iteahome/travelbackend/TravelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.iteahome.travelbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
