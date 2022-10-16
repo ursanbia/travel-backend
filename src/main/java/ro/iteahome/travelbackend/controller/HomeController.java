21d2a23eeafa181e63406dac51c8b2c1a2302b50
radubaciuDell <radumbaciu@gmail.com>
2022-10-16 13:19:40 +0300

    added controller

---
+++ src/main/java/ro/iteahome/travelbackend/controller/HomeController.java
@@ -0,0 +1,13 @@
+package ro.iteahome.travelbackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping ("/")
+    public String welcome(){
+        return "Welcome to Travel";
+    }
+}
